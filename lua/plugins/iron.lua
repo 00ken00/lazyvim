9c16e7b1769a7b9e012f02c7437c0fdbbcd9f1b4
Ken <veryshj123@gmail.com>
2024-09-01 23:27:45 +0900

    Update keyboard

--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -1,6 +1,6 @@
-if true then
-  return {}
-end
+-- if true then
+--   return {}
+-- end
 
 return {
   "Vigemus/iron.nvim",
@@ -17,7 +17,7 @@ return {
             command = { "zsh" },
           },
           python = {
-            command = { "ipython" }, -- or { "ipython", "--no-autoindent" }
+            command = { "ipython", "--no-autoindent" }, -- or { "ipython", "--no-autoindent" }
             format = require("iron.fts.common").bracketed_paste_python,
           },
         },
@@ -28,20 +28,20 @@ return {
       -- Iron doesn't set keymaps by default anymore.
       -- You can set them here or manually add keymaps to the functions in iron.core
       keymaps = {
-        send_motion = "<space>sc",
-        visual_send = "<space>sc",
-        send_file = "<space>sf",
-        send_line = "<space>sl",
-        send_paragraph = "<space>sp",
-        send_until_cursor = "<space>su",
-        send_mark = "<space>sm",
-        mark_motion = "<space>mc",
-        mark_visual = "<space>mc",
-        remove_mark = "<space>md",
-        cr = "<space>s<cr>",
-        interrupt = "<space>s<space>",
-        exit = "<space>sq",
-        clear = "<space>cl",
+        send_motion = "<space>rs",
+        visual_send = "<space>rr",
+        send_file = "<space>rf",
+        send_line = "<space>rl",
+        send_paragraph = "<space>rr",
+        send_until_cursor = "<space>ru",
+        send_mark = "<space>rm",
+        -- mark_motion = "<space>mc",
+        -- mark_visual = "<space>mc",
+        -- remove_mark = "<space>md",
+        cr = "<space>r<cr>",
+        interrupt = "<space>r<space>",
+        exit = "<space>rq",
+        clear = "<space>rc",
       },
       -- If the highlight is on, you can change how it looks
       -- For the available options, check nvim_set_hl
@@ -50,6 +50,11 @@ return {
       },
       ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
     })
+
+    vim.keymap.set("n", "<space>re", "<cmd>IronRepl<cr>")
+    vim.keymap.set("n", "<space>rt", "<cmd>IronRestart<cr>")
+    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
+    -- vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
   end,
 }
 
